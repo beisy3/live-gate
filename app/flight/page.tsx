5f95fe03bb601b239807352c3588f5a5b30df8bc
beisy3 <arisulz111@gmail.com>
2025-12-25 15:30:54 +0000

    connected to db

--- app/flight/page.tsx
+++ app/flight/page.tsx
@@ -1,12 +1,32 @@
 import flightresponse from '@/Type/flight_data';
 import dummy_data from '@/dummy.js';
-      
-
+import clientPromise from '@/lib/mongodb';
+  
 export default async function Page({searchParams} : {searchParams: Promise<{flight_number: string, departure_city: string}>}) {
 
     const params = await searchParams;
     const { flight_number, departure_city } = params;
 
+    let x
+    try{
+      const client = await clientPromise;
+      const db = client.db('live-gate');
+      x = await db.collection('gate').find({'flight_number': flight_number, 'departure_airport' : departure_city}).toArray()
+      if(!x.length){
+        console.log('fetching_data')
+        //find new information
+      }
+      else{
+        console.log(x[0].departure_airport)
+        console.log('x', x)
+      }
+      
+    } catch(err){
+      console.log(err)
+      return(
+        <div>Error connecting to database</div>
+      )
+    }
     /*const response = await fetch(`https://api.aviationstack.com/v1/timetable?iataCode=${departure_city}&type=departure&airline_iata=${flight_number}&access_key=f5994424a847443c2b6bfc9582bfb8f1`)
     const data  : flightresponse = await response.json();*/
     const data : flightresponse = dummy_data;
